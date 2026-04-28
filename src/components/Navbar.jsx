bc39537eb0d785fba827b7b338c787a1b957d86f
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-04-29 00:05:31 +0600

    module running

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,16 @@ import userIcon from "./../assets/user.png";
 import { AuthContext } from "../provider/AuthProvider";
 
 const Navbar = () => {
-  const { user } = use(AuthContext);
+  const { user, logOut } = use(AuthContext);
+  const handleLogout = () => {
+    logOut()
+      .then(() => {
+        alert("user logout successfully");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
   return (
     <div className="flex justify-between items-center">
       <div>{user && user.email}</div>
@@ -15,9 +24,16 @@ const Navbar = () => {
       </div>
       <div className="login-btn flex gap-5">
         <img src={userIcon} alt="" />
-        <Link to="/auth/login" className="btn btn-primary px-10">
-          Login
-        </Link>
+        {user ? (
+          <button onClick={handleLogout} className="btn btn-primary px-10">
+            {" "}
+            Logout
+          </button>
+        ) : (
+          <Link to="/auth/login" className="btn btn-primary px-10">
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
---
+++ src/pages/NewsDetails.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+
+const NewsDetails = () => {
+  return (
+    <div>
+      <header className="py-3">
+        <Header></Header>
+      </header>
+      <main className="w-11/12 mx-auto "></main>
+    </div>
+  );
+};
+
+export default NewsDetails;
